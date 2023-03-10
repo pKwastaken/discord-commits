74751e1f71afcdb379363a3d1246c93aaf04b221
itz-coffee <gm1003.gm@gmail.com>
2023-03-09 23:11:00 -0600

    Typescript rewrite

---
+++ src/index.ts
@@ -0,0 +1,66 @@
+import { context } from "@actions/github"
+import * as core from "@actions/core"
+import fetch from "node-fetch"
+import { PushEvent } from "@octokit/webhooks-definitions/schema"
+import { generateText, obfuscate } from "./utils"
+
+const url = core.getInput("webhookUrl").replace("/github", "")
+const data = context.payload as PushEvent
+
+const sender = data.sender!.login
+const repo = data.repository!.name
+const branch = context.ref.replace("refs/heads/", "")
+const senderUrl = `${data.sender!.html_url}`
+const repoUrl = `${data.repository!.html_url}`
+const branchUrl = `${repoUrl}/tree/${branch}`
+
+const originalFooter = `[${repo}](<${repoUrl}>)/[${branch}](<${branchUrl}>)`
+const privateFooter = `${obfuscate(repo)}/${obfuscate(branch)}`
+
+let isPrivate = false
+const footer = () =>
+	`- [${sender}](<${senderUrl}>) on ${
+		isPrivate ? privateFooter : originalFooter
+	}`
+
+let buffer = new String()
+
+async function send(): Promise<void> {
+	const content = buffer + footer()
+	const res = await fetch(url, {
+		method: "POST",
+		body: JSON.stringify({
+			username: sender,
+			avatar_url: data.sender.avatar_url,
+			content: content
+		}),
+		headers: { "Content-Type": "application/json" }
+	})
+
+	if (!res.ok) core.setFailed(await res.text())
+
+	buffer = new String()
+}
+
+async function run(): Promise<void> {
+	if (context.eventName !== "push") return
+
+	for (const commit of data.commits) {
+		let [text, _private] = generateText(commit)
+
+		if (_private) isPrivate = true
+
+		const sendLength = text.length + buffer.length + footer().length
+
+		if (sendLength >= 2000) {
+			await send()
+			isPrivate = false
+		}
+
+		buffer += text
+	}
+
+	if (buffer.length > 0) await send()
+}
+
+run()
---
+++ src/utils.ts
@@ -0,0 +1,39 @@
+import { Commit } from "@octokit/webhooks-definitions/schema"
+
+const blocks = ["▂", "▄", "▆", "█"]
+
+export function obfuscate(input: string): string {
+	let output = ""
+
+	for (let i = 0; i < input.length; i++) {
+		const char = input.charAt(i)
+
+		if (char.match(/\S+/)) {
+			const rand = Math.random() * blocks.length
+			output += blocks[Math.trunc(rand)]
+		} else {
+			output += char
+		}
+	}
+
+	return output
+}
+
+export function generateText(commit: Commit): [string, boolean] {
+	const id = commit.id.substring(0, 8)
+	const repo = commit.url.split("/commit")[0]
+
+	let text = `[\`${id}\`]`
+	let message = commit.message
+	let isPrivate = false
+
+	if (message.startsWith("!") || message.startsWith("$")) {
+		isPrivate = true
+		text += `() ${obfuscate(message.substring(1).trim())}`
+	} else {
+		text += `(<${repo}/commit/${id}>) ${message}`
+	}
+
+	text += "\n"
+	return [text, isPrivate]
+}
